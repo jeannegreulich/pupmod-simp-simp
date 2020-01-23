ed249e91ea2f75ba163b5681f2c49a6c253b0596
jeannegreulich <jeanne.greulich@onyxpoint.com>
2020-01-23 08:21:53 -0500

    (SIMP-7529) Disable SSSD local provider EL8
    - SSSD V2 does not contain a local provider by default.  Set
      configuring the local provider (simp::sssd::client::local_provider) to
      false for EL8.
    - Allow sssd to be configured without domain in EL8 only. This might be
      a desired configuration in EL8 because of the performance enhancements
      of using sssd to look up local files as opposed to nss.
    - Deprecate the sssd client autofs, ssh and sudo settings.  The sssd
      module will configure the services if the service is configured
      to be used  in sssd:services.
    - Update Spec helpers to allow unit tests to run on FIPS enable hosts.
    
    SIMP-7529 #close
    SIMP-7386 #close

---
+++ spec/classes/00_classes/sssd/client_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+
+shared_examples_for 'sssd client' do
+  it { is_expected.to compile.with_all_deps }
+
+  it { is_expected.to contain_class('sssd') }
+  it { is_expected.to contain_class('sssd::service::pam') }
+  it { is_expected.to contain_class('sssd::service::nss') }
+end
+
+describe 'simp::sssd::client' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, os_facts|
+      context "on #{os}" do
+        let(:facts) { os_facts }
+
+        if os_facts[:kernel] == 'windows'
+          it { expect{ is_expected.to compile.with_all_deps }.to raise_error(/'windows .+' is not supported/) }
+        else
+          context 'with default parameters' do
+            it_should_behave_like 'sssd client'
+            if os_facts[:os][:release][:major] <= '7'
+              it { is_expected.to contain_sssd__domain('LOCAL')}
+            else
+              it { is_expected.to_not contain_sssd__domain('LOCAL')}
+            end
+            it { is_expected.to_not contain_sssd__domain('LDAP')}
+            it { is_expected.to contain_class('sssd::service::autofs') }
+            it { is_expected.to contain_class('sssd::service::sudo') }
+            it { is_expected.to contain_class('sssd::service::ssh') }
+          end
+
+          context 'with alternate params' do
+            let(:params) {{
+              :ldap_domain       => true,
+              :autofs            => false,
+              :sudo              => false,
+              :ssh               => false,
+              :enumerate_users   => true,
+              :cache_credentials => false,
+              :min_id            => 501
+            }}
+            it_should_behave_like 'sssd client'
+            if os_facts[:os][:release][:major] <= '7'
+              it { is_expected.to contain_sssd__domain('LOCAL').with({
+                'min_id' => 501,
+                'enumerate' => false,
+                'cache_credentials' => false
+                })
+              }
+            else
+              it { is_expected.to_not contain_sssd__domain('LOCAL')}
+            end
+            it { is_expected.to_not contain_class('sssd::service::autofs') }
+            it { is_expected.to_not contain_class('sssd::service::sudo') }
+            it { is_expected.to_not contain_class('sssd::service::ssh') }
+            it { is_expected.to contain_sssd__domain('LDAP').with({
+              'min_id' => 501,
+              'enumerate' => true,
+              'cache_credentials' => false
+              })
+            }
+          end
+        end
+      end
+    end
+  end
+end
